1e3be4fa4ffc6d5ec8277e44d592b0e49d7ac8ff
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-09-02 22:35:07 -0500

    Terminado

--- ExpresionesRegulares/MainForm.Designer.cs
+++ ExpresionesRegulares/MainForm.Designer.cs
@@ -31,24 +31,20 @@
             this.textBoxEntrada = new System.Windows.Forms.TextBox();
             this.buttonValidr = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.labelMessage = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBoxEntrada
             // 
-            this.textBoxEntrada.Location = new System.Drawing.Point(38, 134);
+            this.textBoxEntrada.Location = new System.Drawing.Point(38, 47);
             this.textBoxEntrada.Name = "textBoxEntrada";
             this.textBoxEntrada.Size = new System.Drawing.Size(330, 20);
             this.textBoxEntrada.TabIndex = 0;
             // 
             // buttonValidr
             // 
-            this.buttonValidr.Location = new System.Drawing.Point(412, 131);
+            this.buttonValidr.Location = new System.Drawing.Point(401, 47);
             this.buttonValidr.Name = "buttonValidr";
             this.buttonValidr.Size = new System.Drawing.Size(75, 23);
             this.buttonValidr.TabIndex = 1;
@@ -65,71 +61,38 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "Validador de:";
             // 
-            // label2
+            // comboBox1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(57, 22);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(81, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "HH:MM am, pm";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Hora en formato 12 hrs",
+            "Hora en formato 24 hrs",
+            "Fracciones de decimales",
+            "CURP",
+            "IP",
+            "MAC"});
+            this.comboBox1.Location = new System.Drawing.Point(110, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(186, 21);
+            this.comboBox1.TabIndex = 9;
             // 
-            // label3
+            // labelMessage
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(57, 35);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(61, 13);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "HH:MM hrs";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(57, 48);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(42, 13);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "5.6/6.4";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(57, 61);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(37, 13);
-            this.label5.TabIndex = 6;
-            this.label5.Text = "CURP";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(57, 74);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(17, 13);
-            this.label6.TabIndex = 7;
-            this.label6.Text = "IP";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(57, 87);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(30, 13);
-            this.label7.TabIndex = 8;
-            this.label7.Text = "MAC";
+            this.labelMessage.AutoSize = true;
+            this.labelMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMessage.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.labelMessage.Location = new System.Drawing.Point(35, 104);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(0, 20);
+            this.labelMessage.TabIndex = 10;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(505, 184);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelMessage);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonValidr);
             this.Controls.Add(this.textBoxEntrada);
@@ -145,12 +108,8 @@
         private System.Windows.Forms.TextBox textBoxEntrada;
         private System.Windows.Forms.Button buttonValidr;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label labelMessage;
     }
 }
 
--- ExpresionesRegulares/MainForm.cs
+++ ExpresionesRegulares/MainForm.cs
@@ -20,13 +20,24 @@ namespace ExpresionesRegulares
         private Regex fracciones;
         private Regex horaAmPm;
         private Regex hora;
+        int opcion;
 
 
 
         public MainForm()
         {
             ip = new Regex(@"([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}");
-            fracciones = new Regex(@"\-?\d+(\.\d)?(\/\-?\d+(\.\d+)?)");
+            fracciones = new Regex(@"(\d+(\.\d)?)/(\d+(\.\d)?)");
+            horaAmPm = new Regex(@"((0[0-9])|(1[0-2])):([0-5][0-9])\s((am)|(pm))");
+            hora = new Regex(@"(0[0-9])|(1[0-9])|(2[0-3]):([0-5][0-9])\shrs");
+            curp = new Regex(@"[A-Z]{1}[AEIOU]{1}[A-Z]{2}[0-9]{2}" +
+                                "(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])" +
+                                "[HM]{1}" +
+                                "(AS|BC|BS|CC|CS|CH|CL|CM|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE)" +
+                                "[B-DF-HJ-NP-TV-Z]{3}" +
+                                "[0-9A-Z]{1}[0-9]{1}");
+            mac = new Regex(@"(([0-9]|[A-F]){2}:){5}([0-9]|[A-F]){2}");
+            opcion = 0;
             InitializeComponent();
         }
 
@@ -34,11 +45,71 @@ namespace ExpresionesRegulares
         {
             string entrada = textBoxEntrada.Text;
 
-            //switch (entrada)
-            //{
-            //    case 1:
-            //        break;
-            //}
+            opcion = comboBox1.SelectedIndex;
+
+            switch (opcion)
+            {
+                case 0:
+                    if (horaAmPm.IsMatch(entrada))
+                    {
+                        labelMessage.Text = "Hora en formato 12 hrs correcto";
+                    }
+                    else
+                    {
+                        labelMessage.Text = "Hora en formato 12 hrs incorrecto!";
+                    }
+                    break;
+                case 1:
+                    if (hora.IsMatch(entrada))
+                    {
+                        labelMessage.Text = "Hora en formato 24 hrs correcto";
+                    }
+                    else
+                    {
+                        labelMessage.Text = "Hora en formato 24 hrs incorrecto!";
+                    }
+                    break;
+                case 2:
+                    if (fracciones.IsMatch(entrada))
+                    {
+                        labelMessage.Text = "Fracción correcta";
+                    }
+                    else
+                    {
+                        labelMessage.Text = "Fracción incorrecta!";
+                    }
+                    break;
+                case 3:
+                    if (curp.IsMatch(entrada))
+                    {
+                        labelMessage.Text = "CURP correcto";
+                    }
+                    else
+                    {
+                        labelMessage.Text = "CURP incorrecto!";
+                    }
+                    break;
+                case 4:
+                    if (ip.IsMatch(entrada))
+                    {
+                        labelMessage.Text = "IP correcto";
+                    }
+                    else
+                    {
+                        labelMessage.Text = "IP incorrecto!";
+                    }
+                    break;
+                case 5:
+                    if (mac.IsMatch(entrada))
+                    {
+                        labelMessage.Text = "MAC correcto";
+                    }
+                    else
+                    {
+                        labelMessage.Text = "MAC incorrecto!";
+                    }
+                    break;
+            }
         }
     }
 }
