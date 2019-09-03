c9e34eda035be9fd8b7ccf1490280a3e03d22378
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-09-02 19:38:48 -0500

    Primeras expresiones

--- ExpresionesRegulares/MainForm.cs
+++ ExpresionesRegulares/MainForm.cs
@@ -14,8 +14,19 @@ namespace ExpresionesRegulares
 {
     public partial class MainForm : Form
     {
+        private Regex ip;
+        private Regex mac;
+        private Regex curp;
+        private Regex fracciones;
+        private Regex horaAmPm;
+        private Regex hora;
+
+
+
         public MainForm()
         {
+            ip = new Regex(@"([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}");
+            fracciones = new Regex(@"\-?\d+(\.\d)?(\/\-?\d+(\.\d+)?)");
             InitializeComponent();
         }
 
